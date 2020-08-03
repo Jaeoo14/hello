283502324c6131264aeb5eb36124c1fc0d764738
admin <admin@DESKTOP-IU26KS5>
2020-08-03 11:12:33 +0900

    Initial commit

---
+++ First/src/hello/Hellojava.java
@@ -0,0 +1,22 @@
+/*
+ * Date : 2020년06월26일
+ * Author : 권병욱
+ * 설명 : 첫번째 자바 프로그램 입니다.
+ * Version : 1.0
+ */
+
+package hello;
+
+public class Hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow, Java");
+		/*
+		  
+		 
+		 */
+	}		
+
+}
+
+
---
+++ First/src/hello/hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("ss.");
+		System.out.println("ee.");
+		System.out.println("ww.");
+		System.out.println("qq dd.");
+		System.out.println("eeㅠㅠ.");
+		System.out.println("ff.");
+		System.out.println("dd.");
+	
+	}
+}
